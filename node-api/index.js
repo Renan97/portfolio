a770f5697e0748b521535beaaecff074e7378cfd
rensantos <rs.ls2408@gmail.com>
2022-02-17 09:45:06 -0300

    initial

---
+++ node-api/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+//Middleware
+app.use(express.json()); // bodyparser json
+app.use(express.urlencoded({ extended: false })); //bodyparser forms
+
+//API routing
+app.use("/api/users", require("./routes/users"));
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Running on port: " + PORT));
